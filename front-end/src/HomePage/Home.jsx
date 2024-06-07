f077cf412f1da44bafab162cc74b851016eb0fa3
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-06-07 23:28:26 +0100

    "Added Partners component to Home page"

--- front-end/src/HomePage/Home.jsx
+++ front-end/src/HomePage/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Header } from "../Header/Header";
 import HeroSection from "../HeroSection/HeroSection";
 import Services from "../OurServices/services";
+import Partners from "../partners/Partners";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
         <Header />
         <HeroSection/>
         <Services/>
+        <Partners/>
     </>
 
   );
