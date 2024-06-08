23b3575ad74a33ab472db1fdbf0a3eba8f2a4c51
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-06-08 10:38:21 +0100

    "Added search functionality to Missing component with input field and filtering of missing people data"

--- front-end/src/Missing/Missing.jsx
+++ front-end/src/Missing/Missing.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import { Box, Heading, Text, Button, Image, Flex } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, Image, Flex, useMenuPositioner, Input } from "@chakra-ui/react";
 import { missingPeople } from "../missingPeople";
 import { Header } from "../Header/Header";
+import { useState } from "react";
 
 const Missing = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+      };
+
+    const filteredMissing = missingPeople.filter(miss => {
+        const MissingCin = miss?.cin ?? '';
+        const MissingName = miss?.fullName?.toLowerCase() ?? '';
+        const searchLower = searchTerm.toLowerCase();
+        return MissingCin.includes(searchLower) || MissingName.includes(searchLower);
+      });
+
+
   return (
     <>
     <Header/>
+    <Input
+          type="text"
+          placeholder="Search by CIN or Name"
+          value={searchTerm}
+          onChange={handleSearch}
+          className="mb-4 w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+        />
     <Flex wrap={'wrap'} p={5} justify={'center'}>
-      {missingPeople.map((person) => (
+      {filteredMissing.map((person) => (
         <Box
           transform={'scale(0.8)'}
         //   m={}
